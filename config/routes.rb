4aae25cd5c38cc8a51a66a511f5e80d8170f80c6
Denis Aleshin <snorik.net@gmail.com>
2012-01-26 20:11:46 +0400

    WiP: feature search-template

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ MxData::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match 'test' => 'welcome#test'
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
